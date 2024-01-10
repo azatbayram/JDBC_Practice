9c57ddf40035fc2607a5d6424b3743b2c33eb23a
azatbayram <bayramov.azat.tkm@gmail.com>
2024-01-11 02:19:12 +0300

    send get request spartanURL + "/api/hello"

--- src/test/java/api_tests/SpartanGetRequest.java
+++ src/test/java/api_tests/SpartanGetRequest.java
@@ -6,6 +6,8 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.when;
+
 public class SpartanGetRequest {
 
     String spartanURL = "http://3.85.214.90:8000";
@@ -37,5 +39,30 @@ public class SpartanGetRequest {
         Assert.assertEquals(response.contentType(), "application/json; charset=UFT-8");
 
         Assert.assertTrue(response.body().asString().contains("Fidole"));
+    }
+
+    /*
+    Given no headers provided
+    When users send get request to /api/hello
+    Then response status code should be 200
+    And Content type header should be "text/plain;charset=UTF-8"
+    And header should contain date
+    And Content-Length should be 17
+    And body shoud be "Hello from Sparta"
+     */
+
+    @Test
+    public void test3(){
+        Response response = when().get(spartanURL + "/api/hello");
+
+        Assert.assertEquals(response.statusCode(), 200);
+        Assert.assertEquals(response.contentType(),"text/plain;charset=UTF-8");
+        Assert.assertTrue(response.headers().hasHeaderWithName("Date"));
+        Assert.assertEquals(response.header("Content-Length"),"17");
+
+        //System.out.println(response.headers().toString());
+        Assert.assertEquals(response.body().asString(), "Hello from Sparta");
+
+
     }
 }
