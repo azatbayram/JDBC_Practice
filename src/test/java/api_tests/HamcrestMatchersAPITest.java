654ad6414b7490a68082e04faa154d6822a165a2
azatbayram <bayramov.azat.tkm@gmail.com>
2024-01-12 06:59:30 +0300

    one spartan with HamcrestMatchers

---
+++ src/test/java/api_tests/HamcrestMatchersAPITest.java
@@ -0,0 +1,41 @@
+package api_tests;
+
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class HamcrestMatchersAPITest {
+
+    /*
+      given accept type is Json
+      And path param id is 15
+      When user sends a get request to spartans/{id}
+      Then status code is 200
+      And content type is Json
+      And json data has following
+          "id": 15,
+          "name": "Meta",
+          "gender": "Female",
+          "phone": 1938695106
+       */
+
+    @Test
+    public void OneSpartanWithHamcrest(){
+
+        given().accept(ContentType.JSON)
+                .and().pathParam("id",15)
+                .when().get("http://3.85.214.90:8000/api/spartans/{id}")
+                .then().statusCode(200)
+                .and().assertThat().contentType("application/json")
+                .and().assertThat().body("id",equalTo(15),
+                        "name",equalTo("Meta"),
+                        "gender",equalTo("Female"),
+                        "phone",equalTo(1938695106));
+
+
+    }
+
+}
