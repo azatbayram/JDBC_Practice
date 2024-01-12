9046c5e5d2c035afbec22e54c02dea69fd697a22
azatbayram <bayramov.azat.tkm@gmail.com>
2024-01-12 04:44:58 +0300

    JsonPath findAll and max syntex

--- src/test/java/api_tests/HRTestWithJsonPath.java
+++ src/test/java/api_tests/HRTestWithJsonPath.java
@@ -36,4 +36,26 @@ public class HRTestWithJsonPath {
         List<String> countryNamesWithRegionId2 = jsonPath.getList("items.findAll {it.region_id==2}.country_name");
         System.out.println(countryNamesWithRegionId2);
     }
+
+
+    @Test
+    public void test2(){
+        Response response = given().queryParam("limit",107)
+                .when().get("/employees");
+
+        JsonPath jsonPath = response.jsonPath();
+
+        //get me all email of employees who is working as IT_PROG
+        List<String> firstNames = jsonPath.getList("items.findAll {it.job_id==\"IT_PROG\"}.first_name");
+        System.out.println(firstNames);
+
+        //get me all firstname of emplyoees who is making more than 10000
+        List<String> firstNames2 = jsonPath.getList("items.findAll {it.salary >10000}.first_name");
+        System.out.println(firstNames2);
+
+        //get me first name of who is making highest salary
+        String kingName = jsonPath.getString("items.max {it.salary}.first_name");
+        System.out.println("kingName = " + kingName);
+
+    }
 }
