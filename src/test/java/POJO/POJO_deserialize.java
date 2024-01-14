3781f5d425ac73fc82c6c8875626d99dbbb6ce0b
azatbayram <bayramov.azat.tkm@gmail.com>
2024-01-14 07:52:45 +0300

    regions to pojo

--- src/test/java/POJO/POJO_deserialize.java
+++ src/test/java/POJO/POJO_deserialize.java
@@ -5,6 +5,8 @@ import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.testng.Assert.*;
 public class POJO_deserialize {
@@ -29,6 +31,26 @@ public class POJO_deserialize {
 
         assertEquals(spartan15.getId(), 15);
         assertEquals(spartan15.getName(), "Meta");
+
+    }
+
+    @Test
+    public void regionToPojo(){
+
+        Response response = when().get("http://3.85.214.90:1000/ords/hr/regions");
+        assertEquals(response.statusCode(),200);
+
+        //JSON to POJO
+        Regions regions=response.body().as(Regions.class);
+
+        System.out.println("regions.getHasMore() = " + regions.getHasMore());
+        System.out.println("regions.getCount() = " + regions.getCount());
+
+        System.out.println(regions.getItems().get(0).getRegionName());
+
+        List<Item> items = regions.getItems();
+
+        System.out.println(items.get(1).getRegionName());
 
     }
 }
