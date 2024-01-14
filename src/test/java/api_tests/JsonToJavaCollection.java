635728189914341ef2f55321c97b06a4ce76ae7e
azatbayram <bayramov.azat.tkm@gmail.com>
2024-01-14 06:43:46 +0300

    convert all spartan json data to List of Map

--- src/test/java/api_tests/JsonToJavaCollection.java
+++ src/test/java/api_tests/JsonToJavaCollection.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
@@ -35,6 +36,26 @@ public class JsonToJavaCollection {
 
         BigDecimal phone=new BigDecimal(String.valueOf(jsonDataMap.get("phone")));
         System.out.println("phone = " + phone);
+
+    }
+
+    @Test
+    public void allSpartansToListOfMap(){
+
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/api/spartans");
+
+        assertEquals(response.statusCode(),200);
+
+        //we need to de-serialize Json response to List of Maps
+        List<Map<String,Object>> allSpartanList=response.body().as(List.class);
+        System.out.println("allSpartanList = " + allSpartanList);
+
+        //print second spartan first name
+        System.out.println(allSpartanList.get(1).get("name"));
+        //save spartan 3 in a map
+        Map<String,Object> spartan3=allSpartanList.get(2);
+        System.out.println(spartan3);
 
     }
 
