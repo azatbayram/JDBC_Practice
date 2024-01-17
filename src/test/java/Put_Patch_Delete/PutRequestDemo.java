af5c9a66be2d8456dfbea2c530352dd03db67990
azatbayram <bayramov.azat.tkm@gmail.com>
2024-01-17 07:42:50 +0300

    put request test

---
+++ src/test/java/Put_Patch_Delete/PutRequestDemo.java
@@ -0,0 +1,55 @@
+package Put_Patch_Delete;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.*;
+
+public class PutRequestDemo {
+
+    @BeforeClass
+    public void beforeClass(){
+        baseURI = ConfigurationReader.get("spartan_api_url");
+    }
+
+    @Test
+    public void test1(){
+
+        Map<String, Object> putRequestBody = new HashMap<>();
+        putRequestBody.put("name", "Azat");
+        putRequestBody.put("gender", "Male");
+        putRequestBody.put("phone", 123456789l);
+
+        given().log().all()
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .pathParam("id", 90)
+                .and()
+                .body(putRequestBody)
+                .when()
+                .put("/api/spartan/{id}")
+                .then().log().all()
+                .assertThat().statusCode(201);
+
+        System.out.println("========================================================");
+
+        //send get request to verify body
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id",90)
+                .when()
+                .get("/api/spartans/{id}");
+
+        assertEquals(response.statusCode(),200);
+        response.prettyPrint();
+    }
+
+}
